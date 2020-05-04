2b224f396059bc7d82ed374cc608ef55822c9086
Mauricio Massaia <mauriciomassaia@gmail.com>
2020-05-04 18:05:47 +1000

    First commit

---
+++ readme.md
@@ -0,0 +1,30 @@
+# pixi-trailing-texture
+
+![./screenshot.png](./screenshot.png)
+
+### Scripts
+
+Install dependencies:
+```
+npm i
+```
+
+Start development:
+```
+npm start
+```
+
+Access via http://localhost:3000
+
+Build
+```
+npm run build
+```
+
+### Deploy gh-pages
+
+To deploy `gh-pages` I followed this [awesome article](https://medium.com/linagora-engineering/deploying-your-js-app-to-github-pages-the-easy-way-or-not-1ef8c48424b7). Thanks [Christophe Hamerling](https://github.com/chamerling)
+
+---
+
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import { Application, RenderTexture, Sprite, Graphics, filters } from 'pixi.js'
+
+const WIDTH = window.innerWidth
+const HEIGHT = window.innerHeight
+
+const app = new Application({
+  width: WIDTH,
+  height: HEIGHT,
+  resolution: window.devicePixelRatio || 1,
+  antialias: true,
+  backgroundColor: 0xffffff
+})
+
+const { stage } = app
+const mouse = { x: 0, y: 0 }
+
+const stripes = new Graphics()
+
+const total = 10
+const w = WIDTH / total
+const h = HEIGHT / total
+
+for (var i = 0; i < total; i++) {
+  stripes
+    // top left half
+    .beginFill(i % 2 === 0 ? 0xff0000 : 0x00ff00, 1)
+    .moveTo(i * w, 0)
+    .lineTo((i + 1) * w, 0)
+    .lineTo(0, h * (i + 1))
+    .lineTo(0, h * i)
+
+    // bottom right half
+    .beginFill(i % 2 === 0 ? 0xff0000 : 0x00ff00, 1)
+    .moveTo(WIDTH, h * i)
+    .lineTo(WIDTH, h * (i + 1))
+    .lineTo(w * (i + 1), HEIGHT)
+    .lineTo(w * i, HEIGHT)
+    .lineTo(WIDTH, h * i)
+}
+
+stage.addChild(stripes)
+
+const fadeTexture = RenderTexture.create(WIDTH, HEIGHT)
+const fadeSprite = new Sprite(fadeTexture)
+fadeSprite.alpha = 0.95
+stage.addChild(fadeSprite)
+
+const bgTexture = RenderTexture.create(WIDTH, HEIGHT)
+const blurFilter = new filters.BlurFilter()
+blurFilter.blurX = 20
+blurFilter.blurY = 20
+
+const bgSprite = new Sprite(bgTexture)
+bgSprite.filters = [blurFilter]
+stage.addChild(bgSprite)
+
+const circle = new Graphics()
+circle.beginFill(0x000000).drawCircle(0, 0, 50)
+circle.filters = [blurFilter]
+stage.addChild(circle)
+
+app.ticker.add(() => {
+  if (Math.abs(mouse.x - circle.x) > 2) {
+    blurFilter.blur = Math.min(30, blurFilter.blur + 0.5)
+  } else {
+    blurFilter.blur = Math.max(2, blurFilter.blur - 1)
+  }
+
+  circle.x += (mouse.x - circle.x) * 0.25
+  circle.y += (mouse.y - circle.y) * 0.25
+
+  app.renderer.render(bgSprite, fadeTexture, true)
+  app.renderer.render(circle, fadeTexture, false)
+
+  app.renderer.render(fadeSprite, bgTexture, true)
+})
+
+function onMouseMove ({ clientX, clientY }) {
+  mouse.x = clientX
+  mouse.y = clientY
+}
+
+window.onload = () => {
+  window.addEventListener('mousemove', onMouseMove, false)
+  document.body.appendChild(app.view)
+  app.start()
+}
