8947fcc47d0d2e9de2abf4ee7fa657d88ae433f2
Mauricio Massaia <mauriciomassaia@gmail.com>
2022-01-05 17:19:59 +1100

    Update prototype with new colors and interaction

--- src/index.js
+++ src/index.js
@@ -2,52 +2,34 @@ import { Application, RenderTexture, Sprite, Graphics, filters } from 'pixi.js'
 
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
+const COLORS = [
+  0xfefefe, // white
+  0x7a7a78, // grey
+  0xfbcb00, // yellow
+  0xFF6B6B, // red
+  0xFF9999, // pink
+  0xBDB4F0,
+  0xC9BFBF
+]
 
 const app = new Application({
   width: WIDTH,
   height: HEIGHT,
-  resolution: window.devicePixelRatio || 1,
+  resolution: 1,
   antialias: true,
   backgroundColor: 0xffffff
 })
 
 const { stage } = app
 const mouse = { x: 0, y: 0 }
+let currentColorIndex = 1
 
-const stripes = new Graphics()
-
-const total = 10
-const w = WIDTH / total
-const h = HEIGHT / total
-const stripeColor1 = 0x7a7a78
-const stripeColor2 = 0xfbcb00
-
-for (var i = 0; i < total; i++) {
-  stripes
-    // top left half
-    .beginFill(i % 2 === 0 ? stripeColor1 : stripeColor2, 1)
-    .moveTo(i * w, 0)
-    .lineTo((i + 1) * w, 0)
-    .lineTo(0, h * (i + 1))
-    .lineTo(0, h * i)
-
-    // bottom right half
-    .beginFill(i % 2 === 0 ? stripeColor1 : stripeColor2, 1)
-    .moveTo(WIDTH, h * i)
-    .lineTo(WIDTH, h * (i + 1))
-    .lineTo(w * (i + 1), HEIGHT)
-    .lineTo(w * i, HEIGHT)
-    .lineTo(WIDTH, h * i)
-}
-
-stage.addChild(stripes)
-
-const fadeTexture = RenderTexture.create(WIDTH, HEIGHT)
+const fadeTexture = RenderTexture.create({ width: WIDTH, height: HEIGHT })
 const fadeSprite = new Sprite(fadeTexture)
-fadeSprite.alpha = 0.95
+fadeSprite.alpha = 0.99
 stage.addChild(fadeSprite)
 
-const bgTexture = RenderTexture.create(WIDTH, HEIGHT)
+const bgTexture = RenderTexture.create({ width: WIDTH, height: HEIGHT })
 const blurFilter = new filters.BlurFilter()
 blurFilter.blurX = 20
 blurFilter.blurY = 20
@@ -57,33 +39,51 @@ bgSprite.filters = [blurFilter]
 stage.addChild(bgSprite)
 
 const circle = new Graphics()
-circle.beginFill(0x000000).drawCircle(0, 0, 50)
 circle.filters = [blurFilter]
 stage.addChild(circle)
 
 app.ticker.add(() => {
   if (Math.abs(mouse.x - circle.x) > 2) {
-    blurFilter.blur = Math.min(30, blurFilter.blur + 0.5)
+    blurFilter.blur = Math.min(20, blurFilter.blur + 0.5)
   } else {
     blurFilter.blur = Math.max(2, blurFilter.blur - 1)
   }
-
-  circle.x += (mouse.x - circle.x) * 0.25
-  circle.y += (mouse.y - circle.y) * 0.25
+  circle.rotation += (mouse.x - mouse.y) * 0.05
+  circle.x += (mouse.x - circle.x) * 0.5
+  circle.y += (mouse.y - circle.y) * 0.5
 
   app.renderer.render(bgSprite, fadeTexture, true)
   app.renderer.render(circle, fadeTexture, false)
-
   app.renderer.render(fadeSprite, bgTexture, true)
 })
 
-function onMouseMove ({ clientX, clientY }) {
+const changeColor = () => {
+  // draw big circle with current color
+  circle
+    .clear()
+    .beginFill(COLORS[currentColorIndex])
+    .drawCircle(50, 30, 100)
+
+  // change to next color
+  currentColorIndex++
+  currentColorIndex %= COLORS.length
+
+  // draw small circle with new color
+  circle
+    .beginFill(COLORS[currentColorIndex])
+    .drawCircle(2, 0, 50)
+}
+
+const onMouseMove = ({ clientX, clientY }) => {
   mouse.x = clientX
   mouse.y = clientY
 }
 
 window.onload = () => {
   window.addEventListener('mousemove', onMouseMove, false)
+  window.addEventListener('pointerdown', changeColor)
+
   document.body.appendChild(app.view)
+  changeColor()
   app.start()
 }
